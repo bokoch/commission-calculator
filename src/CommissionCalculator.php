eb46cee82e9c999188ee3f90d0f5e0632d795910
Mykola Bokoch <bokoch1995@gmail.com>
2023-08-23 22:39:36 +0300

    Project init. Installed guzzle, phpunit

---
+++ src/CommissionCalculator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Bokoch\CommissionCalculator;
+
+class CommissionCalculator
+{
+
+}
---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+abstract class AbstractTestCase extends TestCase
+{
+    //
+}
